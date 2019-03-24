2fbb310ff7a7ac42884d8f75f8ece06282fe4b2f
Gilon <zochowski.gilbert@gmail.com>
2019-03-24 13:30:22 +0100

    Some message

---
+++ src/main/java/calculate/BMICalculator.java
@@ -0,0 +1,20 @@
+package calculate;
+
+public class BMICalculator implements Calculate {
+
+    private int weight;
+    private int height;
+
+    public BMICalculator(int height, int weight) {
+    }
+
+    @Override
+    public String calculate() {
+        return "";
+    }
+
+    @Override
+    public String interpret() {
+        return "";
+    }
+}
---
+++ src/main/java/calculate/Calculate.java
@@ -0,0 +1,6 @@
+package calculate;
+
+public interface Calculate {
+    String calculate();
+    String interpret();
+}
